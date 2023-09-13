59c39bd2a0bc8a13dad7468ee805e245871f15f2
root <Nicolaslegoat>
2023-09-13 01:08:36 -0700

    betty pb

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,8 +6,8 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node && node->left == NULL && node->right == NULL)
-        return (1);
+	if (node && node->left == NULL && node->right == NULL)
+		return (1);
 
-    return(0);
+	return(0);
 }
