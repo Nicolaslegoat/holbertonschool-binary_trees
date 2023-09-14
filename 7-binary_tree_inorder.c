93285369d775245ad744bd7d9479a080861f378f
root <Nicolaslegoat>
2023-09-14 01:25:29 -0700

    betty pb

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,13 +4,13 @@
  * @tree: a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to call for each node
  */
- void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if ((tree != NULL) & (func != NULL))
-    {
-        binary_tree_inorder(tree->left, func);
-        func(tree->n);
-        binary_tree_inorder(tree->right, func);
-    }
+	if ((tree != NULL) & (func != NULL))
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
 
 }
